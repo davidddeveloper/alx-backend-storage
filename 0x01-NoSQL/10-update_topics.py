a906a8526216fce6615e8301515f3dd9762cf640
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-08-28 12:25:40 +0000

    Change school topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+    10-update_topics.py: changes all topics of a school document
+    based on the name
+
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+        change school topics base on the name
+        name: the school name to update
+        topics: list of topics 
+
+    """
+    mongo_collection.update({name: name}, {'$push', {topics: topics}})
