a78b7bac18b6a74ed96923ce1e1b546b5722b7d5
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-08-28 13:20:07 +0000

    log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -11,21 +11,23 @@ from pymongo import MongoClient
 def log():
     """log stats from mongodb"""
 
-    client = MongoClient()  # client
+    client = MongoClient()  # establish connection
     db = client.logs  # database
     c_nginx = db.nginx  # nginx collection
 
     doc_count = c_nginx.count_documents({})
 
+    # count logs for each method
     get_count = c_nginx.count_documents({"method": "GET"})
     post_count = c_nginx.count_documents({"method": "POST"})
     put_count = c_nginx.count_documents({"method": "PUT"})
     patch_count = c_nginx.count_documents({"method": "PATCH"})
     delete_count = c_nginx.count_documents({"method": "DELETE"})
 
-    status_check_count = c_nginx.count_documents({"path": "/status", "method": "GET"})
+    status_check_count = c_nginx.count_documents(
+        {"path": "/status", "method": "GET"})
 
-    # print logs
+    # print statistic
     print(doc_count)
     print("Methods:")
     print("    method GET: {}".format(get_count))
@@ -35,4 +37,6 @@ def log():
     print("    method DELETE: {}".format(delete_count))
     print("{} status check".format(status_check_count))
 
-log()
+
+if __name__ == '__main__':
+    log()
