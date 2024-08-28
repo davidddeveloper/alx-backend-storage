b73ec29fd2064d2019056b1da306e59650e543fc
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-08-28 13:13:08 +0000

    log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,27 +7,32 @@
 
 from pymongo import MongoClient
 
-
 client = MongoClient()  # client
 db = client.logs  # database
 c_nginx = db.nginx  # nginx collection
 
-doc_count = c_nginx.count_documents({})
 
-get_count = c_nginx.count_documents({"method": "GET"})
-post_count = c_nginx.count_documents({"method": "POST"})
-put_count = c_nginx.count_documents({"method": "PUT"})
-patch_count = c_nginx.count_documents({"method": "PATCH"})
-delete_count = c_nginx.count_documents({"method": "DELETE"})
+def log():
+    """log stats"""
 
-status_check_count = c_nginx.count_documents({"path": "/status", "method": "GET"})
+    doc_count = c_nginx.count_documents({})
 
-# print logs
-print(doc_count)
-print("Methods:")
-print("    method GET: {}".format(get_count))
-print("    method POST: {}".format(post_count))
-print("    method PUT: {}".format(put_count))
-print("    method PATCH: {}".format(patch_count))
-print("    method DELETE: {}".format(delete_count))
-print("{} status check".format(status_check_count))
+    get_count = c_nginx.count_documents({"method": "GET"})
+    post_count = c_nginx.count_documents({"method": "POST"})
+    put_count = c_nginx.count_documents({"method": "PUT"})
+    patch_count = c_nginx.count_documents({"method": "PATCH"})
+    delete_count = c_nginx.count_documents({"method": "DELETE"})
+
+    status_check_count = c_nginx.count_documents({"path": "/status", "method": "GET"})
+
+    # print logs
+    print(doc_count)
+    print("Methods:")
+    print("    method GET: {}".format(get_count))
+    print("    method POST: {}".format(post_count))
+    print("    method PUT: {}".format(put_count))
+    print("    method PATCH: {}".format(patch_count))
+    print("    method DELETE: {}".format(delete_count))
+    print("{} status check".format(status_check_count))
+
+log()
